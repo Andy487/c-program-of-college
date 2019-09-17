223a4c159f38fe368e77db338ad508259243d978
Anand Nayak <54777475+Andy487@users.noreply.github.com>
2019-09-17 14:49:33 +0530

    Add files via upload

---
+++ STAR'NUM.C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int t,i,j;
+    clrscr();
+	 t=1;
+	 for(i=1;i<=5;i++)
+       {
+	 for(j=1;j<=i;j++)
+
+	   printf("%d",t);
+	    t++;
+
+	 {
+	   printf("\n");
+	 }
+       }
+       getch();
+       }
