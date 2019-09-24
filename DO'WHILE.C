6f4f3095ead891578a83378244b68390a2e48fab
Anand Nayak <54777475+Andy487@users.noreply.github.com>
2019-09-25 00:39:58 +0530

    Add files via upload

---
+++ DO'WHILE.C
@@ -0,0 +1,48 @@
+	#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+void main()
+{
+     int a,b,c,choice;
+     char ch;
+     ch='N';
+     clrscr();
+     printf("enter two number");
+     scanf("%d %d",&a,&b);
+     printf("1.addition \n");
+     printf("2.subtraction \n");
+     printf("3.multiplaction \n");
+     printf("4.division \n");
+     do
+     {
+     printf("enter your choice \n");
+     scanf("%d",&choice);
+     switch(choice)
+      {
+	  case 1:
+	      c=a+b;
+	      printf("addition is %d\n",c);
+	      break;
+     case 2:
+	      c=a-b;
+	      printf("subtraction is %d\n",c);
+	       break;
+     case 3:
+	     c=a*b;
+	     printf("muliplaction is %d\n",c);
+	     break;
+     case 4:
+	     c=a/b;
+	     printf("division is %d\n",c);
+	     break;
+	  default:
+	   printf("entered wrong choice \n");
+      }
+       printf("press 'Y'for continue");
+       fflush(stdin);
+       scanf("%c",&ch);
+
+   }  while(ch=='Y');
+
+     getch();
+}
